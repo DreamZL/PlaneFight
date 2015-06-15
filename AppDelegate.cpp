12b1b6121076caf1cc756de3709f1cdf2e851dda
赵龙 <okmxsw456@vip.qq.com>
2015-06-15 09:10:04 +0800

    Create AppDelegate.cpp

---
+++ AppDelegate.cpp
@@ -0,0 +1,58 @@
+#include "AppDelegate.h"
+#include "LayerGameStart.h"
+#include "SimpleAudioEngine.h"
+using namespace CocosDenshion;
+
+
+USING_NS_CC;
+
+AppDelegate::AppDelegate() {
+
+}
+
+AppDelegate::~AppDelegate() 
+{
+}
+
+bool AppDelegate::applicationDidFinishLaunching() {
+    // initialize director
+    auto director = Director::getInstance();
+    auto glview = director->getOpenGLView();
+    if(!glview) {
+        glview = GLView::create("Plane Fight");
+        director->setOpenGLView(glview);
+		glview->setFrameSize(480,800);
+    }
+	glview->setDesignResolutionSize(480,800,ResolutionPolicy::EXACT_FIT);//一定要放置在director->setOpenGLView(glview);这句话之后
+
+    // turn on display FPS
+    director->setDisplayStats(true);
+
+    // set FPS. the default value is 1.0/60 if you don't call this
+    director->setAnimationInterval(1.0 / 60);
+
+    // create a scene. it's an autorelease object
+    //auto scene = HelloWorld::createScene();
+	auto scene = LayerGameStart::scene();
+
+    // run
+    director->runWithScene(scene);
+
+    return true;
+}
+
+// This function will be called when the app is inactive. When comes a phone call,it's be invoked too
+void AppDelegate::applicationDidEnterBackground() {
+    Director::getInstance()->stopAnimation();
+
+    // if you use SimpleAudioEngine, it must be pause
+     SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
+}
+
+// this function will be called when the app is active again
+void AppDelegate::applicationWillEnterForeground() {
+    Director::getInstance()->startAnimation();
+
+    // if you use SimpleAudioEngine, it must resume here
+    SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
+}
